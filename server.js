27f2d4daf41a6e5d45988257e216dcb0608c137c
cmwschroeder <cmwschroeder@gmail.com>
2022-04-26 10:45:20 -0700

    changing port so that heroku hosting will work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 // Serve images, css files, js files from the public directory
 // Allows us to reference files with their relative path
