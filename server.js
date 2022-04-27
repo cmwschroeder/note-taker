df0b00566a468571b04aae68ad5896dcc37928d4
cmwschroeder <cmwschroeder@gmail.com>
2022-04-27 10:39:30 -0700

    added getting the notes from the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +16,14 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+// app.post('/api/notes', (req, res) =>{
+
+// });
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
