779a2c3401c83da30ff196685d10a549ff6dc9ba
cmwschroeder <cmwschroeder@gmail.com>
2022-04-27 17:28:51 -0700

    started post, get request and break it up in body, added middleware, gave note unique id

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const notes = require("./db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serve images, css files, js files from the public directory
-// Allows us to reference files with their relative path
-// Example: http://localhost:3000/images/cat.jpg
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
@@ -21,9 +21,24 @@ app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
-// app.post('/api/notes', (req, res) =>{
+app.post('/api/notes', (req, res) =>{
+  //deconstruct the request body into title and text so we can write them to our database.
 
-// });
+  //make sure that there is a body
+  if(req.body) {
+    const { title, text } = req.body;
+
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    console.log(newNote);
+
+    res.json("Note added");
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
